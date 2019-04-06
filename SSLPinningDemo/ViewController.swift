0b0818fb814fa72e34c61e0b223a6d0897f2a65a
Chander bhushan <cbhushan1995@gmail.com>
2019-04-06 11:41:48 +0530

    Initial Commit

---
+++ SSLPinningDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSLPinningDemo
+//
+//  Created by chander bhushan on 06/04/19.
+//  Copyright © 2019 Educational. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
